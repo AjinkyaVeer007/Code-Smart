5084559f32ba0879cbcd8dc37bfb878f650b8dab
AjinkyaVeer <veerajinkya1@gmail.com>
2023-06-03 21:43:53 +0530

    MM

--- src/Components/test.jsx
+++ src/Components/test.jsx
@@ -2,63 +2,31 @@ import React from "react";
 
 function Test() {
   return (
-    <div className="bg-white w-[100vw] h-[100vh]">
-      <nav class="bg-red-800 w-full mx-2">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex items-center justify-between h-16">
-            <div class="flex items-center">
-              <div class="flex-shrink-0">
-                <img
-                  class="h-16 w-16"
-                  src="https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/5a2493173e6664f9ef97833287856a5b.svg"
-                  alt="Logo"
-                />
-              </div>
-              <div class="ml-4 text-white text-lg font-semibold hidden sm:block">
-                Company Name
-              </div>
-            </div>
-            <div class="sm:hidden">
-              <button
-                type="button"
-                class="text-white hover:bg-red-700 focus:outline-none focus:text-white focus:bg-red-700"
-              >
-                <svg
-                  class="h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-            <div class="hidden sm:block">
-              <a
-                href="#"
-                class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:text-white focus:bg-red-700"
-              >
-                Contact Us
-              </a>
-            </div>
-          </div>
+    <div className="bg-orange-100 w-[100vw] h-[100vh]">
+      <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold text-gray-900 mb-8">
+          Coding in Progress
+        </h1>
+        <h2 class="text-2xl font-semibold text-gray-700 mb-4">
+          by Ajinkya Veer
+        </h2>
+        <div class="animate-bounce">
+          <svg
+            class="h-12 w-12 text-gray-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </div>
-        <div class="sm:hidden">
-          <div class="px-2 pt-2 pb-3 space-y-1">
-            <a
-              href="#"
-              class="block px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:text-white focus:bg-red-700"
-            >
-              Contact Us
-            </a>
-          </div>
-        </div>
-      </nav>
+      </div>
     </div>
   );
 }
